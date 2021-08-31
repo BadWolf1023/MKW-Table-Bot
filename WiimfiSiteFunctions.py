6c5de491fac9e199037011bac823b3a56f13e734
Bad Wolf <willglyles@gmail.com>
2021-08-31 07:47:34 -0500

    Remove Race Testing Room additions

--- WiimfiSiteFunctions.py
+++ WiimfiSiteFunctions.py
@@ -21,7 +21,9 @@ mkwxURL = 'https://wiimmfi.de/stats/mkwx'
 submkwxURL = f"{mkwxURL}/list/"
 special_test_cases = {f"{submkwxURL}r0000000":("Special room: Room has times with high deltas and a race with times that are the same as another race's times", "testing_rooms/SameTimeHighDelta.html"),
 f"{submkwxURL}r0000001":("Table Bot Challenge Room One", "testing_rooms/TableBotTestOne.html"),
-f"{submkwxURL}r0000002":("Table Bot Challenge Room Two", "testing_rooms/TableBotTestTwo.html")}
+f"{submkwxURL}r0000002":("Table Bot Challenge Room Two", "testing_rooms/TableBotTestTwo.html"),
+f"{submkwxURL}r0000003":("Table Bot Remove Race Test w/ quickedit", "testing_rooms/removerace_one.html"),
+f"{submkwxURL}r0000004":("Table Bot Remove Race Test w/ quickedit, 2nd room to merge", "testing_rooms/removerace_two.html")}
 
 
 async def fetch(session, url):
