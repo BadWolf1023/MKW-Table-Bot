636aada69f46d3ff4dd75e19007221596ede2c4e
Bad Wolf <willglyles@gmail.com>
2021-10-29 04:30:49 -0500

    Unit Testing for Query Builder module
    Normally wouldn't do unit testing, but I felt dynamically building strings to be used in queries called for verification of correctness

---
+++ data_tracking/UnitTesting.py
@@ -0,0 +1,18 @@
+'''
+Created on Oct 29, 2021
+
+@author: willg
+'''
+import unittest
+from data_tracking.Data_Tracker_SQL_Queury_Builder import *
+class TestPlayerSQLStatements(unittest.TestCase):
+
+    def test_prepared_fcs(self):
+        result = get_fcs_in_Player_table([1, 2, 3])
+        self.assertEqual(result, """SELECT fc
+FROM Player
+WHERE fc in (?, ?, ?);""", f"get_fcs_in_Player_table did not produce expected result")
+        
+if __name__ == '__main__':
+    unittest.main()
+    
