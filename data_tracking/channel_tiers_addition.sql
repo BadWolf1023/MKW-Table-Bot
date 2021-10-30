2a43c62e8586e9e271e796234a3a46f3445046ea
Bad Wolf <willglyles@gmail.com>
2021-10-30 07:40:25 -0500

    SQL Refactor + added is_ct data

--- data_tracking/channel_tiers_addition.sql
+++ data_tracking/channel_tiers_addition.sql
@@ -33,63 +33,38 @@ CT_TABLE_BOT_CHANNEL_TIER_MAPPINGS = {
     }    
 */
 
-/* Delete any channels that are no longer in our channel_id to tier mapping */
-DELETE FROM Tier
-WHERE channel_id NOT IN (843981870751678484,
-					    836652527432499260,
-					    747290199242965062,
-					    747290182096650332,
-					    873721400056238160,
-					    747290167391551509,
-					    801620685826818078,
-					    747290151016857622,
-					    801620818965954580,
-					    805860420224942080,
-					    747290132675166330,
-					    754104414335139940,
-					    801630085823725568,
-					    747289647003992078,
-					    747544598968270868,
-					    781249043623182406,
-					    875532532383363072,
-					    850520560424714240,
-					    801625226064166922,
-					    747290436275535913,
-					    879429019546812466,
-					    747290415404810250,
-					    747290383297282156,
-					    823014979279519774,
-					    747290363433320539,
-					    871442059599429632);
-						    
+/* Deletes all registered tiers */
+DELETE FROM Tier;
+			
+/*Add channel_ids mapping to tiers*/			    
 /* RT Table Bot Channels */
-REPLACE INTO Tier (channel_id, tier) VALUES(843981870751678484, 8);
-REPLACE INTO Tier (channel_id, tier) VALUES(836652527432499260, 7);
-REPLACE INTO Tier (channel_id, tier) VALUES(747290199242965062, 6);
-REPLACE INTO Tier (channel_id, tier) VALUES(747290182096650332, 5);
-REPLACE INTO Tier (channel_id, tier) VALUES(873721400056238160, 5);
-REPLACE INTO Tier (channel_id, tier) VALUES(747290167391551509, 4);
-REPLACE INTO Tier (channel_id, tier) VALUES(801620685826818078, 4);
-REPLACE INTO Tier (channel_id, tier) VALUES(747290151016857622, 3);
-REPLACE INTO Tier (channel_id, tier) VALUES(801620818965954580, 3);
-REPLACE INTO Tier (channel_id, tier) VALUES(805860420224942080, 3);
-REPLACE INTO Tier (channel_id, tier) VALUES(747290132675166330, 2);
-REPLACE INTO Tier (channel_id, tier) VALUES(754104414335139940, 2);
-REPLACE INTO Tier (channel_id, tier) VALUES(801630085823725568, 2);
-REPLACE INTO Tier (channel_id, tier) VALUES(747289647003992078, 1);
-REPLACE INTO Tier (channel_id, tier) VALUES(747544598968270868, 1);
-REPLACE INTO Tier (channel_id, tier) VALUES(781249043623182406, 1);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(843981870751678484, 8, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(836652527432499260, 7, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(747290199242965062, 6, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(747290182096650332, 5, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(873721400056238160, 5, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(747290167391551509, 4, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(801620685826818078, 4, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(747290151016857622, 3, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(801620818965954580, 3, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(805860420224942080, 3, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(747290132675166330, 2, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(754104414335139940, 2, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(801630085823725568, 2, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(747289647003992078, 1, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(747544598968270868, 1, 0);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(781249043623182406, 1, 0);
 
 /* CT Table Bot Channels */
-REPLACE INTO Tier (channel_id, tier) VALUES(875532532383363072, 7);
-REPLACE INTO Tier (channel_id, tier) VALUES(850520560424714240, 6);
-REPLACE INTO Tier (channel_id, tier) VALUES(801625226064166922, 5);
-REPLACE INTO Tier (channel_id, tier) VALUES(747290436275535913, 4);
-REPLACE INTO Tier (channel_id, tier) VALUES(879429019546812466, 4);
-REPLACE INTO Tier (channel_id, tier) VALUES(747290415404810250, 3);
-REPLACE INTO Tier (channel_id, tier) VALUES(747290383297282156, 2);
-REPLACE INTO Tier (channel_id, tier) VALUES(823014979279519774, 2);
-REPLACE INTO Tier (channel_id, tier) VALUES(747290363433320539, 1);
-REPLACE INTO Tier (channel_id, tier) VALUES(871442059599429632, 1);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(875532532383363072, 7, 1);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(850520560424714240, 6, 1);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(801625226064166922, 5, 1);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(747290436275535913, 4, 1);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(879429019546812466, 4, 1);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(747290415404810250, 3, 1);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(747290383297282156, 2, 1);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(823014979279519774, 2, 1);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(747290363433320539, 1, 1);
+INSERT INTO Tier (channel_id, tier, is_ct) VALUES(871442059599429632, 1, 1);
 
 COMMIT;
