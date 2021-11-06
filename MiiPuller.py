4f7d2d876f95c86327ef27041944d26a0bd7cf90
Bad Wolf <willglyles@gmail.com>
2021-11-06 17:18:19 -0500

    Attempt to pull multiple miis at once

--- MiiPuller.py
+++ MiiPuller.py
@@ -13,6 +13,7 @@ import miirender
 #import requests
 import common
 from datetime import timedelta, datetime
+from typing import Set, List, Dict
 
 from xml.dom.minidom import parse, parseString
 
@@ -28,15 +29,16 @@ SAKE_POST_DATA ="""<?xml version="1.0" encoding="UTF-8"?>
       <ns1:secretKey>9Rmy</ns1:secretKey>
       <ns1:loginTicket>23c715d620f986c22Pwwii</ns1:loginTicket>
       <ns1:tableid>FriendInfo</ns1:tableid>
-      <ns1:filter>ownerid={}</ns1:filter>
+      <ns1:filter>{}</ns1:filter>
       <ns1:sort>recordid</ns1:sort>
       <ns1:offset>0</ns1:offset>
-      <ns1:max>1</ns1:max>
+      <ns1:max>{}</ns1:max>
       <ns1:surrounding>0</ns1:surrounding>
       <ns1:ownerids></ns1:ownerids>
       <ns1:cacheFlag>0</ns1:cacheFlag>
       <ns1:fields>
         <ns1:string>info</ns1:string>
+        <ns1:string>ownerid</ns1:string>
       </ns1:fields>
     </ns1:SearchForRecords>
   </SOAP-ENV:Body>
@@ -57,6 +59,7 @@ BACK_OFF_SECONDS_AMOUNT = 20
 DYNAMIC_CACHER_COOLDOWN = timedelta(hours=2)
 MAXIMUM_FAILURES = 30
 MII_DYNAMIC_CACHER = [datetime.now(), 0, MII_DEFAULT_CACHE_TIME] #date of last reset
+from copy import copy
 
 def update_pulling_mii_failed():
     MII_DYNAMIC_CACHER[1] += 1
@@ -75,19 +78,23 @@ def reset_dynamic_cacher_if_needed():
 def cache_time_expired(last_access_time):
     return (datetime.now() - last_access_time) > MII_DYNAMIC_CACHER[2]
 
-def mii_cache_update(fc, mii_bytes, mii_hex_str):
-    MII_CACHE[fc] = [(mii_bytes, mii_hex_str), datetime.now()]
+def update_mii_cache(mii_datas):
+    for fc, (mii_bytes, mii_hex_str) in mii_datas.items():
+        MII_CACHE[fc] = [(mii_bytes, mii_hex_str), datetime.now()]
 
-def get_mii_data_if_cached(fc):
+def get_unexpired_cached_mii_datas(fcs:Set[str]) -> Set[str]:
     if MII_DYNAMIC_CACHER[1] > MAXIMUM_FAILURES:
-        return None, None, True
-    if fc in MII_CACHE:
-        if not cache_time_expired(MII_CACHE[fc][1]):
-            return *MII_CACHE[fc][0], True
-    return None, None, False
+        return None, None
+    result = set()
+    for fc in fcs:
+        if fc in MII_CACHE:
+            if not cache_time_expired(MII_CACHE[fc][1]):
+                result.add(fc)
+    return result, (len(result) == len(fcs))
 
-def get_sake_post_data(player_id):
-    return SAKE_POST_DATA.format(player_id)
+def get_sake_post_data(player_ids):
+    player_id_args = [f"ownerid={pid}" for pid in player_ids]
+    return SAKE_POST_DATA.format(" || ".join(player_id_args), len(player_ids))
 
 def fix_fc_text(fc:str) -> int:
     #Takes an FC in the format xxxx-xxxx-xxxx and converts it into a usable int for wiifc
@@ -111,36 +118,60 @@ def wiifc(pid, id4) -> Tuple[int, int]:
 def mii_data_is_corrupt(mii_data:str):
     return mii_data == b''
 
-async def get_mii_data_from_pid(pid:int) -> str:
+def format_sake_xml_response(sake_response):
+    return parseString(sake_response)
+
+async def get_mii_data_for_pids(pids:Dict[int, str]) -> str:
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.post(wiimmfi_sake, headers=SAKE_HEADERS, data=get_sake_post_data(pid), ssl=common.sslcontext, timeout=REQUEST_TIME_OUT_SECONDS) as data:
-                miidatab64 = str(await data.content.read())
-                miidatahex = base64.b64decode(miidatab64[399:527])
-                encode = binascii.hexlify(miidatahex)
-                if mii_data_is_corrupt(encode):
-                    update_pulling_mii_failed()
-                    return None, None
-                return (miidatahex, str(encode)[2:-1])
+            async with session.post(wiimmfi_sake, headers=SAKE_HEADERS, data=get_sake_post_data(pids), ssl=common.sslcontext, timeout=REQUEST_TIME_OUT_SECONDS) as data:
+                sake_response = str(await data.content.read())
+                print(sake_response)
+                sake_response = format_sake_xml_response(sake_response)
+                print(sake_response)
+                #miidatahex = base64.b64decode(miidatab64[399:527])
+                #encode = binascii.hexlify(miidatahex)
+                #if mii_data_is_corrupt(encode):
+                #    update_pulling_mii_failed()
+                #    return None
+                #return (miidatahex, str(encode)[2:-1])
     except:
         update_pulling_mii_failed()
-        return None, None
+        return None
 
-async def get_mii_data(fc:str):
-    pid, _ = wiifc(fix_fc_text(fc), b'RMCJ')
-    return await get_mii_data_from_pid(pid)
+async def get_mii_data_for_fcs(fcs:Set[str]):
+    pids_mapping = {wiifc(fix_fc_text(fc), b'RMCJ')[0]:fc for fc, _ in fcs}
+    mii_datas = await get_mii_data_for_pids(pids_mapping)
+    if mii_datas is None:
+        return None
+    result = {}
+    for pid in mii_datas:
+        result[pids_mapping[pid]] = mii_datas[pid]
+    return result
 
-async def get_miis(fc:str, message_id:str, picture_width=512):
+async def get_miis(fcs:List[str], message_id:str, picture_width=512):
     reset_dynamic_cacher_if_needed()
-    mii_bytes, mii_hex_str, should_use_cache = get_mii_data_if_cached(fc)
-    if not should_use_cache:
-        mii_bytes, mii_hex_str = await get_mii_data(fc)
-        mii_cache_update(fc, mii_bytes, mii_hex_str)
-        
-    if mii_bytes is None:
+    if len(fcs) == 0:
+        return {}
+    fcs = set(fcs)
+    
+    fcs_in_cache, all_miis_cached = get_unexpired_cached_mii_datas(fcs)
+    if all_miis_cached is not None or all_miis_cached is False:
+        uncached_mii_fcs = fcs.difference(fcs_in_cache)
+        mii_datas = await get_mii_data_for_fcs(uncached_mii_fcs)
+        if mii_datas is None:
+            return NO_MII_ERROR_MESSAGE
+        update_mii_cache(mii_datas)
+    
+    found_fcs, _ = get_unexpired_cached_mii_datas(fcs)
+    if found_fcs is None:
         return NO_MII_ERROR_MESSAGE
-    else:
+    found_data = {fc:copy(MII_CACHE[fc]) for fc in found_fcs}
         
+    if len(found_data) == 0:
+        return NO_MII_ERROR_MESSAGE
+    
+    for fc, ((mii_bytes, mii_hex_str), _) in found_data.items():
         file_name = message_id + "_" + fc + '.png'
         folder_path = common.MIIS_PATH
         full_download_path = folder_path + file_name
