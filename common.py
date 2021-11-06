ab4861493378dd4cd0c2f6a24af66730504de38c
Bad Wolf <willglyles@gmail.com>
2021-11-06 09:03:01 -0500

    Update common.py

--- common.py
+++ common.py
@@ -32,7 +32,7 @@ MAX_PREFIX_LENGTH = 3
 #current_notification = "Help documentation has been changed so you find what you're looking for quickly. Check it out by running `{SERVER_PREFIX}help`. Server administrators now have more table bot defaults they can set for their server."
 
 #Main loop constants
-in_testing_server = True
+in_testing_server = False
 running_beta = False
 beta_is_real = False
 
