b11e840c6e39f5f2654a4ddd7011b1ec1ad0f7c1
Javi <f.javier.r.m@outlook.com>
2022-01-04 17:07:22 +0100

    Improve cohesion. Functions are only used in start war command.

--- commands/start_war/start_war_command.py
+++ commands/start_war/start_war_command.py
@@ -184,3 +184,46 @@ def getNumGPs(args, defaultGPs=3):
                     else:
                         return index, numGPs
     return -1, defaultGPs
+
+
+valid_suppress_large_time_flags = ["largetime=off", "largetime=no", "largetimes=off", "largetimes=no", "sui=yes",
+                                   "sui=on", "sui=true", "lgt=no", "lgt=off", "psb=yes", "psb=on", "psb=true",
+                                   "professionalseriesbagging=yes", "professionalseriesbagging=true"]
+valid_unsuppress_large_time_flags = ["largetime=yes", "largetime=yes", "largetimes=on", "largetimes=yes", "sui=no",
+                                     "sui=off", "sui=false", "lgt=yes", "lgt=on", "psb=no", "psb=off", "psb=false",
+                                     "professionalseriesbagging=no", "professionalseriesbagging=false"]
+
+
+def getSuppressLargeTimes(args, default_use=False):
+    if len(args) < 4:
+        return -1, default_use
+
+    for index, arg in enumerate(args[3:], 3):
+        if arg.lower().strip() in valid_suppress_large_time_flags:
+            return index, True
+        if arg.lower().strip() in valid_unsuppress_large_time_flags:
+            return index, False
+
+    return -1, default_use
+
+
+valid_mii_flags = ["usemiis=", "usemii=", "miis=", "miinames=", "mii=", "miiname=", 'miiheads=']
+
+
+def getUseMiis(args, default_use=False, default_start_arg=1):
+    if len(args) < 2:
+        return default_use, False, -1
+
+    for valid_flag in valid_mii_flags:
+        for ind, arg in enumerate(args[default_start_arg:], default_start_arg):
+            temp_arg = arg.lower().strip()
+            if len(temp_arg) > len(valid_flag) and temp_arg.startswith(valid_flag):
+                use_miis_input = temp_arg[len(valid_flag):].strip()
+                if use_miis_input in ["y", "yes", "t", "true", 'on']:
+                    return True, True, ind
+                elif use_miis_input in ["n", "no", "false", "f", 'off']:
+                    return False, True, ind
+                else:
+                    return default_use, False, ind
+
+    return default_use, False, -1
