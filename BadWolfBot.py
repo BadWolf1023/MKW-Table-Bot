1cbde8db70f22466f9e1a32d1ef7005256472369
Bad Wolf <willglyles@gmail.com>
2022-01-11 03:51:53 -0600

    Removing miscellaneous legacy code

--- BadWolfBot.py
+++ BadWolfBot.py
@@ -351,9 +351,7 @@ def initialize():
     TagAIShell.initialize()
 
 @client.event
-async def on_message(message: discord.Message):
-    ##########################################################################################At this point, we know the room exists, and we certainly have rLID. We're not sure if we have the roomID yet though.    
-    
+async def on_message(message: discord.Message):    
     if message.author == client.user:
         return
     if message.guild is None:
@@ -603,10 +601,6 @@ async def on_message(message: discord.Message):
                 
             elif args[0] in REMOVE_SHA_TERMS:
                 await commands.BotAdminCommands.remove_sha_track(message, args)
-                
-            elif args[0] in {"aidata"} and (common.is_bad_wolf(message.author) or message.author.id == 267395889423712258):
-                if os.path.exists(TagAIShell.AI_Results_file_name):
-                    await message.channel.send(content="Put in Table Bot directory, and use `TagAIShell.view_AI_results()`", file=discord.File(TagAIShell.AI_Results_file_name))
 
             elif args[0] in RACES_TERMS:
                 await commands.TablingCommands.display_races_played_command(message, this_bot, server_prefix, is_lounge_server)
@@ -740,7 +734,7 @@ async def on_message(message: discord.Message):
         await common.safe_send(message, "Cannot access Wiimmfi's mkwx. I'm either blocked by Cloudflare, or the website is down.")    
         await send_to_503_channel(logging_info)
     except TableBotExceptions.CommandDisabled:
-        await common.safe_send(message, "MKW Table Bot cannot access the website. Wiimm has neglected this situation. There is no path forward unless Wiimm whitelists Table Bot. I suggest you go to <https://forum.wii-homebrew.com>, go to 'User Introductions', and create a post about it.")
+        await common.safe_send(message, "This command has been disabled.")
 
     except:
         common.log_traceback(traceback)
@@ -884,42 +878,7 @@ def pickle_CTGP_region():
             return
         except:
             print("Could not dump pickle for CTGP region for ?ctww. Exception occurred.")
-            
-def do_lounge_name_matching():
-    lounge_name_list_path = "C:/Users/willg/Desktop/all_lounge.txt"
-    import csv
-    dict_data = {}
-    txt_file = "mapping.txt"
-    print("Started building map.")
-    with open(lounge_name_list_path, encoding="utf-8") as f:
-        for name in f:
-            name = name.strip().strip("\n")
-            discord_id = UserDataProcessing.get_DiscordID_By_LoungeName(name).strip("\n")
-            dict_data[name] = discord_id if discord_id != "" else "None"
-    print("Finished building map.")
-    
-    no_space_no_caps_names = [name.replace(" ","").lower().replace("\t","").replace("\n","") for name in dict_data]
-    for name in no_space_no_caps_names:
-        if no_space_no_caps_names.count(name) != 1:
-            print(name)
-    
-    dids = [did for did in dict_data.values() if did != ""]
-    for did in dids:
-        if dids.count(did) != 1:
-            print(did)
-            
-            
-    try:
-        with open(txt_file,'w', encoding="utf-8") as g:
-            for name, discord_id in dict_data.items():
-                g.write(f"{discord_id}\n")
-            
-    except IOError:
-        print("I/O error")
-    
 
-        
-    print("Finished exporting as CSV.")
 
 def destroy_all_tablebots():
     for server_id in table_bots:
@@ -947,7 +906,6 @@ def save_data():
         Stats.backup_files()
         Stats.prune_backups()
         Stats.dump_to_stats_file()
-        do_lounge_name_matching()
 
     print(f"{str(datetime.now())}: Finished saving data")
     
