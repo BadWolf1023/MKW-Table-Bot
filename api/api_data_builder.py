431982258130fe38c08f52bfb1ed2227bb2d9185
Bad Wolf <willglyles@gmail.com>
2022-03-09 00:43:25 -0600

    Add orange team styling

--- api/api_data_builder.py
+++ api/api_data_builder.py
@@ -14,7 +14,8 @@ TEAM_STYLE_FILE = f"{CSS_DATA_PATH}team_score_base.css"
 TABLE_HTML_BUILDER_FILE = f"{HTML_DATA_PATH}full_table_builder.html"
 
 TEAM_STYLES = {"rainbow": f"{CSS_DATA_PATH}team_score_rainbow.css",
-               "neon": f"{CSS_DATA_PATH}team_score_neon.css"
+               "neon": f"{CSS_DATA_PATH}team_score_neon.css",
+               "orange": f"{CSS_DATA_PATH}team_score_orange.css"
 }
 
 
@@ -28,7 +29,7 @@ def restructure_if_needed(team_data):
         return new_team_data
     else:
         return team_data
-def build_team_html(team_data: List[Tuple[str, List]], style="neon"):
+def build_team_html(team_data: List[Tuple[str, List]], style="orange"):
     '''team_data contains the scores for teams in the following format:
     [
         (team_1_name, [ (fc_1, player_data_1), (fc_2, player_data_2) ]),
