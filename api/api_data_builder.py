f387da2f9053a976f8df83cea4cbd4dd9d8f796f
Bad Wolf <willglyles@gmail.com>
2022-03-10 01:54:33 -0600

    Change endpoints table id format (no query)

--- api/api_data_builder.py
+++ api/api_data_builder.py
@@ -33,6 +33,52 @@ def restructure_if_needed(team_data):
         return new_team_data
     else:
         return team_data
+
+def build_full_table_html(team_data: List[Tuple[str, List]], style=None):
+    '''team_data contains the scores for teams in the following format:
+    [
+        (team_1_name, [ (fc_1, player_data_1), (fc_2, player_data_2) ]),
+        (team_2_name, [ (fc_1, player_data_1), (fc_2, player_data_2) ]),
+        (team_3_name, [ (fc_1, player_data_1), (fc_2, player_data_2) ])
+    ]
+
+    team_data is what is returned by ScoreKeeper.get_war_table_DCS
+    player_data is a list that contains various information, such as the player's name, player score each GP, and player score each race    
+    '''
+    team_data = restructure_if_needed(team_data)
+    
+    # Read in base css styling and read in custom styling if it was specified
+    styling = ""
+    with codecs.open(TEAM_STYLE_FILE, "r", "utf-8") as fp:
+        styling = fp.read() + "\n\n"
+    if style in TEAM_STYLES:
+        with codecs.open(TEAM_STYLES[style], "r", "utf-8") as fp:
+            styling += fp.read() + "\n\n"
+
+    # Build the team HTML
+    soup = None
+    with codecs.open(TEAM_HTML_BUILDER_FILE, "r", "utf-8") as fp:
+        soup = BeautifulSoup(fp.read(), "html.parser")
+    try:
+        for id_index, (team_tag, players) in enumerate(team_data, 1):
+            # Add the css styling:
+            soup.head.style.string = styling
+            # Add the team tag at the top of the HTML table:
+            html_tag_attrs = {"class": "team_name", "id": f"team_name_{id_index}"}
+            team_name_html_tag = soup.new_tag('th', attrs=html_tag_attrs)
+            team_name_html_tag.string = team_tag
+            soup.body.table.thead.tr.append(team_name_html_tag)
+            # Add the team score at the bottom of the HTML table:
+            html_tag_attrs = {"class": "team_score", "id": f"team_score_{id_index}"}
+            team_score_html_tag = soup.new_tag('td', attrs=html_tag_attrs)
+            team_score = sum(p[1][1] for p in players if not p[0].strip().startswith('#'))
+            team_score_html_tag.string = str(team_score)
+            soup.body.table.tbody.tr.append(team_score_html_tag)
+        return str(soup)
+    finally:
+        if soup is not None:
+            soup.decompose()
+
 def build_team_html(team_data: List[Tuple[str, List]], style=None):
     '''team_data contains the scores for teams in the following format:
     [
@@ -84,7 +130,7 @@ def build_info_page_html(table_id: int):
     with codecs.open(QUICK_INFO_HTML_BUILDER_FILE, "r", "utf-8") as fp:
         soup = BeautifulSoup(fp.read(), "html.parser")
     try:
-        team_score_html_base_url = f"{api_common.API_URL}{api_common.TEAM_SCORES_HTML_ENDPOINT}?table_id={table_id}"
+        team_score_html_base_url = f"{api_common.API_URL}{api_common.TEAM_SCORES_HTML_ENDPOINT}{table_id}"
         
         team_header_tag = soup.new_tag('h1')
         team_header_tag.string = f"Want a stream table overlay that automatically updates? Developing an app to interact with Table Bot? Read how below!"
@@ -100,7 +146,7 @@ def build_info_page_html(table_id: int):
         soup.body.append(list_tag)
 
         for style in TEAM_STYLES:
-            url_tag = soup.new_tag('a', href=f"{team_score_html_base_url}&style={style}")
+            url_tag = soup.new_tag('a', href=f"{team_score_html_base_url}?style={style}")
             url_tag.string = f"Scores with tags in {style} style"
             li_tag = soup.new_tag('li')
             li_tag.append(url_tag)
