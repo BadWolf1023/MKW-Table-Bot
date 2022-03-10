b3611c89e2f3aad8c77af551a941eae3edd60d19
Bad Wolf <willglyles@gmail.com>
2022-03-10 01:54:43 -0600

    See last commit

--- api/endpoints.py
+++ api/endpoints.py
@@ -12,20 +12,20 @@ def initialize(app_):
     global app
     app = app_
 
-    @app.get(TEAM_SCORES_HTML_ENDPOINT, response_class=HTMLResponse)
+    @app.get(TEAM_SCORES_HTML_ENDPOINT + "{table_id}", response_class=HTMLResponse)
     def get_team_scores_html(table_id: int, style: Optional[str] = Query(None, max_length=15)):
         table_bot = cb_interface.get_table_bot(table_id)
         if table_bot is None:
             raise HTTPException(status_code=404, detail="Table ID not found. Either the table has been reset, or the table ID given does not exist. Table Bot gave you a table ID when you started the table. Use that.")
         return api_data_builder.build_team_html(cb_interface.get_team_score_data(table_bot), style)
 
-    @app.get(TEAM_SCORES_JSON_ENDPOINT)
+    @app.get(TEAM_SCORES_JSON_ENDPOINT + "{table_id}")
     def get_team_scores_json(table_id: int):
         table_bot = cb_interface.get_table_bot(table_id)
         if table_bot is None:
             raise HTTPException(status_code=404, detail="Table ID not found. Either the table has been reset, or the table ID given does not exist. Table Bot gave you a table ID when you started the table. Use that.")
         return cb_interface.get_team_score_data(table_bot)
 
-    @app.get(INFO_ENDPOINT, response_class=HTMLResponse)
+    @app.get(INFO_ENDPOINT + "{table_id}", response_class=HTMLResponse)
     def info_page(table_id: int):
         return api_data_builder.build_info_page_html(table_id)
